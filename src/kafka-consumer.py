36c269eb9d79cdb85a680eeaec5142c2ec0376eb
Rakesh Kumar <rakeshcusat@gmail.com>
2015-08-02 00:32:19 -0700

    Minor update in producer and consumer scripts.

--- src/kafka-consumer.py
+++ src/kafka-consumer.py
@@ -1,15 +1,27 @@
+import signal
+import sys
+
 from kafka import KafkaConsumer
 
 
+def signal_handling(signum, frame):
+    # Terminal gracefully when SIGINT signal is received.
+    sys.exit(0)
+
+
 def main():
+    signal.signal(signal.SIGINT, signal_handling)
+
     # To consume messages
     consumer = KafkaConsumer('test',
+                             client_id='consumer-client-id',
                              group_id='consumer-script',
                              bootstrap_servers=['localhost:9092'],
                              # auto_offset_reset="smallest",
                              auto_commit_enable=True,
                              auto_commit_interval_ms=1000,
                              auto_commit_interval_messages=1)
+
     for message in consumer:
         # message value is raw byte string -- decode if necessary!
         # e.g., for unicode: `message.value.decode('utf-8')`
--- src/kafka-producer.py
+++ src/kafka-producer.py
@@ -1,4 +1,12 @@
+from datetime import datetime
+import sys
+from time import time
+
 from kafka import KeyedProducer, KafkaClient
+
+
+def get_time():
+    return datetime.fromtimestamp(time()).strftime('%Y-%m-%d %H:%M:%S')
 
 
 def main():
@@ -8,8 +16,18 @@ def main():
 
     # Insure that topic exists
     kafka.ensure_topic_exists('test')
-    # Note that the application is responsible for encoding messages to type bytes
-    producer.send_messages(b'test',  b'topic-key', b'Hello from script')
+
+    while True:
+        # Note that the application is responsible for encoding messages to type bytes
+        producer.send_messages(
+            'test',  # topic
+            'topic-key',  # key
+            'Hello from script!! (time: {})'.format(get_time()),  # message
+        )
+        input_str = raw_input("Press enter to send another message, otherwise press 'q' to quit: ")
+
+        if input_str and input_str in "qQ":
+            sys.exit(0)
 
 
 if __name__ == "__main__":
